f8c4a3337d2d8bacad908eb46fdc8b4386d50e77
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-06 14:15:49 +0300

    Update 01_face_dataset.py

--- .github/workflows/01_face_dataset.py
+++ .github/workflows/01_face_dataset.py
@@ -1,38 +1,13 @@
 # step 1
+import numpy as np
+import tensorflow as tf
 import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from keras.optimizers import Adam
-from keras.callbacks import TensorBoard
-
-import numpy as np
-import pandas as pd
+from keras.layers import Conv2D,MaxPooling2D,Dense,Flatten,Dropout
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from keras.layers.normalization import BatchNormalization
+from keras_preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.metrics import roc_curve, auc
-from sklearn.metrics import accuracy_score
-#from keras.utils import np_utils
-import itertools
 ## step 2
-#load dataset
-data = np.load('.github/workflows/ORL_faces.npz') 
-
-# load the "Train Images"
-x_train = data['trainX']
-#normalize every image
-x_train = np.array(x_train,dtype='float32')/255
-
-x_test = data['testX']
-x_test = np.array(x_test,dtype='float32')/255
-
-# load the Label of Images
-y_train= data['trainY']
-y_test= data['testY']
-
-# show the train and test Data format
-print('x_train : {}'.format(x_train[:]))
-print('Y-train shape: {}'.format(y_train))
-print('x_test shape: {}'.format(x_test.shape))
+train_dir="../.github/workflows/Ashay_Kumar/"
