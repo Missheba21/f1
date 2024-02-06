0610001e50c39a8764629790ac99c5747785c62d
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-06 04:53:08 +0300

    Update 01_face_dataset.py

--- .github/workflows/01_face_dataset.py
+++ .github/workflows/01_face_dataset.py
@@ -1,3 +1,4 @@
+# step 1
 import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
@@ -15,3 +16,23 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import accuracy_score
 #from keras.utils import np_utils
 import itertools
+## step 2
+#load dataset
+data = np.load('ORL_faces.npz') 
+
+# load the "Train Images"
+x_train = data['trainX']
+#normalize every image
+x_train = np.array(x_train,dtype='float32')/255
+
+x_test = data['testX']
+x_test = np.array(x_test,dtype='float32')/255
+
+# load the Label of Images
+y_train= data['trainY']
+y_test= data['testY']
+
+# show the train and test Data format
+print('x_train : {}'.format(x_train[:]))
+print('Y-train shape: {}'.format(y_train))
+print('x_test shape: {}'.format(x_test.shape))
