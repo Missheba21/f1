2857582be666344faa280b2af44aa68ff93de785
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-06 03:45:55 +0300

    Create 01_face_dataset.py

---
+++ .github/workflows/01_face_dataset.py
@@ -0,0 +1,17 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import accuracy_score
+from keras.utils import np_utils
+import itertools
