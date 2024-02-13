b195e2de858cf8e857c03a05896389d547e4b605
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-13 03:45:46 +0300

    Update 01_face_dataset.py

--- .github/workflows/01_face_dataset.py
+++ .github/workflows/01_face_dataset.py
@@ -9,6 +9,21 @@ from keras.layers.normalization import BatchNormalization
 from keras_preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import math
+
+#from tf.keras.models import Sequential  # This does not work!
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import InputLayer, Input
+from tensorflow.python.keras.layers import Reshape, MaxPooling2D
+from tensorflow.python.keras.layers import Conv2D, Dense, Flatten
+
+
+
+
 ## step 2
 train_dir="../.github/workflows/Original_Images/"
 generator = ImageDataGenerator()
