0f41bcca3bd2f272bd17d510a82fcafcc44ec141
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-13 03:54:44 +0300

    Update 01_face_dataset.py

--- .github/workflows/01_face_dataset.py
+++ .github/workflows/01_face_dataset.py
@@ -5,7 +5,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Conv2D,MaxPooling2D,Dense,Flatten,Dropout
 import matplotlib.pyplot as plt
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras_preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
