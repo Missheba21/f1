e5e220cea851ae01fe0e9780a09ac7ac2537dfb9
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-02-13 16:11:14 +0300

    Update 01_face_dataset.py

--- .github/workflows/01_face_dataset.py
+++ .github/workflows/01_face_dataset.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.layers import BatchNormalization
 from keras_preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
@@ -16,18 +15,14 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import math
-
 #from tf.keras.models import Sequential  # This does not work!
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import InputLayer, Input
 from tensorflow.python.keras.layers import Reshape, MaxPooling2D
 from tensorflow.python.keras.layers import Conv2D, Dense, Flatten
-
 from PIL import Image
 import os
-
 import cv2
-
 
 # Path to the folder containing image files
 folder_path = "../f1/.github/workflows/Original_Images/Akshay_Kumar/"
